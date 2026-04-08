4f284b7373f62a31fdacd1c86d452b3ffe6a5824
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2026-04-08 09:43:40 +0900

    PWA設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,45 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+        manifest: {
+          name: '設定狙いツール',
+          short_name: '設定狙いツール',
+          description: '設定狙いツール。',
+          theme_color: '#e6eeff',
+          display: 'standalone',
+          icons: [
+            {
+              src: 'icon-64x64.png',
+              sizes: '64x64',
+              type: 'image/png',
+            },
+            {
+              src: 'icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+          ],
+        },
+    }),
   ],
   resolve: {
     alias: {
