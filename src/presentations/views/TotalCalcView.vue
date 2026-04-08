99b0b0c9bf3cd0f0b0f7d7e78280ba0fcfd3b5e7
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2026-04-08 14:25:31 +0900

    入力のしかたを変更

--- src/presentations/views/TotalCalcView.vue
+++ src/presentations/views/TotalCalcView.vue
@@ -2,13 +2,12 @@
 import { ref, reactive } from 'vue';
 import { type MachineStatus } from '@/entites/MachineStatus';
 
-const machines = ref<MachineStatus[]>([]);
-const nextMachine = reactive<MachineStatus>({
+const machines = ref<MachineStatus[]>([{
   machineNo: 0,
   games: 0,
   bigCount: 0,
   regCount: 0,
-});
+}]);
 
 /**
  * 確率計算
@@ -22,55 +21,16 @@ const calcRatio = (games: number, count: number) => {
  * キーダウンイベント
  */
 const onKeyDown = (e: KeyboardEvent) => {
-  if (e.key.toLowerCase() !== 'enter') { return; }
-
   const target = e.target as HTMLInputElement;
   const tr = target.closest('tr');
   if (!tr) { throw new Error('HTML構造崩壊'); }
-  const inputs = tr.querySelectorAll('.forInput > input') ?? [];
-
-  const findNext = (list: NodeListOf<Element>, findTarget: Element): number => {
-    for (let i = 0; i < list.length; i++) {
-      if (list[i] === findTarget) { return i + 1; }
-    }
-    return -1;
-  }
-  
-  let index = findNext(inputs, target);
-  if (index === -1) {
-    alert('Invalid Index');
-    return;
-  }
-
-  if (index < inputs.length) {
-    const input = inputs[index];
-    if (input) {
-      (input as HTMLInputElement).focus();
-    }
-  } else {
-    if (tr.classList.contains('forNext')) {
-      machines.value.push(JSON.parse(JSON.stringify(nextMachine)) as MachineStatus)
-      nextMachine.machineNo = 0;
-      nextMachine.games = 0;
-      nextMachine.bigCount = 0;
-      nextMachine.regCount = 0;
-      const input = tr.querySelectorAll('.forInput > input')[0];
-      if (input) {
-        (input as HTMLInputElement).focus();
-      }
-    } else {
-      const tbody = tr.closest('tbody');
-      if (!tbody) { return; }
-      const trList = tbody.querySelectorAll('tr') || [];
-      const nextIndex = findNext(trList, tr);
-      if (nextIndex === -1) { throw new Error('何かの間違い'); }
-      if (trList[nextIndex]) {
-        const input = trList[nextIndex].querySelectorAll('input')[0] ?? null;
-        if (inputs) {
-          (input as HTMLInputElement).focus();
-        }
-      }
-    }
+  if (tr.querySelector('.lastItem')) {
+    machines.value.push({
+      machineNo: 0,
+      games: 0,
+      bigCount: 0,
+      regCount: 0,
+    });
   }
 };
 </script>
@@ -98,15 +58,7 @@ const onKeyDown = (e: KeyboardEvent) => {
           <td>{{ calcRatio(machine.games, machine.bigCount) }}</td>
           <td>{{ calcRatio(machine.games, machine.regCount) }}</td>
           <td>{{ calcRatio(machine.games, machine.bigCount + machine.regCount) }}</td>
-        </tr>
-        <tr class="forNext">
-          <td class="forInput"><input type="number" v-model="nextMachine.machineNo" pattern="\d*" min=0 @keydown="onKeyDown" /></td>
-          <td class="forInput"><input type="number" v-model="nextMachine.games" pattern="\d*" min=0  @keydown="onKeyDown" /></td>
-          <td class="forInput"><input type="number" v-model="nextMachine.bigCount" pattern="\d*" min=0 @keydown="onKeyDown" /></td>
-          <td class="forInput"><input type="number" v-model="nextMachine.regCount" pattern="\d*" min=0 @keydown="onKeyDown" /></td>
-          <td>1 / {{ calcRatio(nextMachine.games, nextMachine.bigCount) }}</td>
-          <td>1 / {{ calcRatio(nextMachine.games, nextMachine.regCount) }}</td>
-          <td>1 / {{ calcRatio(nextMachine.games, nextMachine.bigCount + nextMachine.regCount) }}</td>
+          <td v-if="i === machines.length - 1" class="lastItem"></td>
         </tr>
       </tbody>
     </table>
